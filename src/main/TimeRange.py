e112f6b3ac00d77f79c3a9ca0ecafe2457c16f37
SamV-42 <sjvander@mtu.edu>
2020-10-30 02:05:19 -0400

    Multithreading\! Also, TimeRange test\!
    Multithreading for eg the DiscordBot run() method, in order to close it after a certain delay. Note this does not run tests in paralle, because I don't want or need to deal with synchronization for a bad recreation of a testing framework.

--- src/main/TimeRange.py
+++ src/main/TimeRange.py
@@ -1,7 +1,6 @@
 
 # I don't think it's worth rolling our own Date class
-from datetime import datetime as Date
-
+import datetime
 
 class TimeRange:
 
@@ -20,20 +19,18 @@ class TimeRange:
     def size(self):
         return self.end - self.start
 
-    __start = None    # type: Date
+    __start = None    # type: datetime.datetime
     __end = None
     # todo: granularity?
 
     def __init__(self, start, end):
         # Sanity-check the input
-        if start is None or not isinstance(start, Date):
+        if start is None or not isinstance(start, datetime.datetime):
             raise ValueError("AvailableTime: start must be a Date (datetime.datetime)")
-        if end is None or not isinstance(end, Date):
+        if end is None or not isinstance(end, datetime.datetime):
             raise ValueError("AvailableTime: end must be a Date (datetime.datetime)")
-        if (start - end) <= 0:  # note: date - date = datetime.timedelta
+        if (end - start) <= datetime.timedelta(0):  # note: date - date = datetime.timedelta
             raise ValueError("AvailableTime: end must be after start")
 
         self.__start = start
         self.__end = end
-
-    # TODO TODO: defining a __less__ and similar functions for comparing these
--- src/tests/sample_tests.py
+++ src/tests/sample_tests.py
@@ -1,3 +1,10 @@
+import multiprocessing
+import time
+import traceback
+
+import datetime
+from src.main.TimeRange import TimeRange
+
 from src.main.DiscordBot import DiscordBot
 
 __tests = []
@@ -9,24 +16,50 @@ def ptest(test, message = None):
     __tests.append([test, message])
     return test
 
+def busy_ptest_f(delay = 2, message = None):
+    def wrapper(test):
+        def proc():
+            p = multiprocessing.Process(target=test, name=message, args=())
+            p.start()
+            time.sleep(delay)
+            p.terminate()
+            p.join()
+        ptest(proc, message)
+        return test
+    return wrapper
+
 # ----
 
-@ptest
+@busy_ptest_f(delay=0.5)
 def testBot():
     TOKEN = open("../../deploy/token.txt", "r").read()
     bot = DiscordBot(TOKEN)
     print(f"Logged in and ready to go!")
     bot.run()
 
-
+@ptest
+def testTimeRange():
+    # note to self: ALWAYS use datetime.datetime.today(), not datetime.date.today(). You'll use a date, not a datetime.
+    #  seems obvious when you write it out like that...
+    tr = TimeRange(datetime.datetime(year=1959, month=2, day=3, hour=12, minute=57), datetime.datetime.today())
+    print(f"The Day The Music Died: {tr.start}")
+    print(f"Now: {tr.end}")
+    print(f"How much time has passed: {tr.size}")
 
 # ----
 
 if __name__ == "__main__":
     import builtins as __builtin__
     _print = __builtin__.print
-    __builtin__.print = lambda i: _print("\t" + i)
+    _replacement_print = lambda i: _print("\t" + i)
+    __builtin__.print = _replacement_print
 
     for f in __tests:
-        _print(f"Starting test: \"{f[1]}\"")
-        f[0]()
+        _print(f"\n########################################\nStarting test: \"{f[1]}\"")
+
+        try:
+            f[0]()
+        except Exception:
+            __builtin__.print = _print
+            traceback.print_exc()
+            __builtin__.print = _replacement_print
