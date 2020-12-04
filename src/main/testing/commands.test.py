c1d444af159381b738355d4b9036a0046e8d4b90
DevvyDoo <devinhall4@gmail.com>
2020-12-04 04:51:19 -0500

    test for modifying events

--- src/main/testing/commands.test.py
+++ src/main/testing/commands.test.py
@@ -3,6 +3,8 @@ from spm_bot.DiscordBot import DiscordBot
 from lib import FakeChannel
 
 # Create a fake discord text channel to use for relaying fake messages to the bot
+from spm_bot.commands.EventAdminCommand import EventAdminCommand
+
 fake_channel = FakeChannel.FakeChannel()
 # Create the bot object but do not login
 bot = DiscordBot(fake_database=True)
@@ -132,6 +134,28 @@ async def permission_admin_event_command(_):
     return expect('permission_admin_event_command', fake_channel.messages[-1], '[TESTING ENV] Only users with the `Event Admin` role can use this command!')
 
 
+# Test command to see if replacing a timeframe for an event works
+async def modify_event_command(_):
+
+    # Create an event
+    await fake_channel.send("!event create 12/6/2020-2:00-PM 12/6/2020-6:00-PM", author=event_admin_author)
+    start_index = fake_channel.messages[-1].find('`')
+    id = fake_channel.messages[-1][start_index+1:start_index+9]
+    # Make sure the event exists
+    if not await bot.database.get_event(id):
+        return exists('create_delete_event_command', 'bot.database.get_event(id)', await bot.database.get_event(id))
+
+    new_start = '12/20/2020-2:00-PM'
+    new_end = '12/24/2020-2:00-PM'
+    # Now update the timeframe
+    await fake_channel.send(f"!event modify {id} {new_start} {new_end}", author=event_admin_author)
+
+    # Verify that we have equal datetimes
+    new_start_dt = await EventAdminCommand.parse_datetime(new_start)
+    new_end_dt = await EventAdminCommand.parse_datetime(new_end)
+    event = await bot.database.get_event(id)
+    return expect('modify_event_command', [event.start, event.end], [new_start_dt, new_end_dt])
+
 if __name__ == "__main__":
     import asyncio
 
@@ -145,7 +169,8 @@ if __name__ == "__main__":
         create_get_event_command,
         create_attempt_delete_event_command,
         create_delete_event_command,
-        permission_admin_event_command
+        permission_admin_event_command,
+        modify_event_command
     ]
 
     # Results of tests after they're ran, maps method -> boolean
